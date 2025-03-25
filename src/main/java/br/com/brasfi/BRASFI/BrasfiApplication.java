663e19186e5d4c47407fd854d010043680022be7
jpmamededs <joaopedrodias@live.com>
2025-03-25 17:17:42 -0300

    initial commit (Spring Initializr)

---
+++ src/main/java/br/com/brasfi/BRASFI/BrasfiApplication.java
@@ -0,0 +1,13 @@
+package br.com.brasfi.BRASFI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasfiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brasfi/BRASFI/BrasfiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brasfi.BRASFI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasfiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

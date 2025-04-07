bf73e5958a685c4606ea5de90b358a1536a1d00a
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2025-04-07 19:13:48 -0300

    estrutura do front criada

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,5 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FeedComponent } from './pages/feed/feed.component';
 
 export const routes: Routes = [
-    
+    {
+        path: '',
+        component: HomeComponent,
+        canActivate: []
+    },
+    {
+        path: 'noticias',
+        component: FeedComponent,
+        canActivate: []
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
 ];

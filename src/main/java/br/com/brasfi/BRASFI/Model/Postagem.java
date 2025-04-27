9c2d60dbd13d64b26e1059c575f43be8d18ef918
João Miccuci <joaomicucci3@gmail.com>
2025-04-27 12:02:20 -0300

    model da postagem

---
+++ src/main/java/br/com/brasfi/BRASFI/Model/Postagem.java
@@ -0,0 +1,48 @@
+package br.com.brasfi.BRASFI.Model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+public class Postagem {
+
+    private long id;
+    private String autor;
+
+    @Enumerated(EnumType.ORDINAL)
+    private TipoPostagem tag;
+
+    private String Titulo;
+    private String paragrafo;
+    private String imagemOuVideo;
+    private String link;
+    private boolean fixado;
+    public static final int LIMITE_PALAVRAS= 300;
+
+    @OneToOne(mappedBy = "postagem", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private User user;
+
+
+
+    @OneToMany(mappedBy = "postagem", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Comentarios> comentarios;
+
+
+
+
+
+
+
+
+
+
+
+}

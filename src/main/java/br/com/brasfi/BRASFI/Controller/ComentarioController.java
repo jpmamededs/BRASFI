e908481d4ea02969bf52efb5f2c9c908a23d05a9
jpmamededs <joaopedrodias@live.com>
2025-04-27 11:45:13 -0300

    feat: Comentario entity (with Spring's JPA annotations also) and ComentarioController's empty class

---
+++ src/main/java/br/com/brasfi/BRASFI/Controller/ComentarioController.java
@@ -0,0 +1,10 @@
+package br.com.brasfi.BRASFI.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ComentarioController {
+
+
+
+}
---
+++ src/main/java/br/com/brasfi/BRASFI/Model/Comment.java
@@ -0,0 +1,26 @@
+package br.com.brasfi.BRASFI.Model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Título")
+    private String titulo;
+
+    @Column(name = "Conteúdo")
+    private String conteudo;
+
+    @Column(name = "Data de postagem")
+    private LocalDateTime dataCriacao;
+
+    @Column(name = "Autor")
+    private String autor;
+
+}

b9592b29627aadc9808251a1d31e6e685da87390
João Miccuci <joaomicucci3@gmail.com>
2025-05-04 15:28:24 -0300

    refactor code

--- src/main/java/br/com/brasfi/BRASFI/Controller/RegistrationController.java
+++ src/main/java/br/com/brasfi/BRASFI/Controller/RegistrationController.java
@@ -17,32 +17,12 @@ public class RegistrationController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+
     @PostMapping(value = "/req/signup", consumes = "application/json")
-    public User createUser(@RequestBody UserDTO userDTO) {
-        User user = new User();
-
-        // Obrigatórios
-        user.setEmail(userDTO.getEmail());
-        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
-        user.setUsername(userDTO.getUserName());
-
-        // Opcionais
-        user.setMidleName(userDTO.getMidleName());
-        user.setBiografia(userDTO.getBiografia());
-        user.setLocalizacao(userDTO.getLocalizacao());
-        if (userDTO.getIdade() != null) user.setIdade(userDTO.getIdade());
-        user.setPhoto(userDTO.getPhoto());
-        user.setLinkInstagram(userDTO.getLinkInstagram());
-        user.setLinkLinkedin(userDTO.getLinkLinkedin());
-        user.setLinkLattes(userDTO.getLinkLattes());
-        user.setLinkWhatsapp(userDTO.getLinkWhatsapp());
-
-        if (userDTO.getTemasDeAtuacao() != null) user.setTemasDeAtuacao(userDTO.getTemasDeAtuacao());
-        if (userDTO.getProfissao() != null) user.setProfissao(userDTO.getProfissao());
-
-        user.setGenero(userDTO.getGenero());
-        user.setRole(userDTO.getRole() != null ? userDTO.getRole() : Role.USER); // padrão
+    public User createUser(@RequestBody User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         return userRepository.save(user);
+        }
+
     }
-}

03c36f81032ae355b8de939c8022687cecb7f132
João Miccuci <joaomicucci3@gmail.com>
2025-05-13 11:50:04 -0300

    feat: iframe incluido

--- front/src/app/pages/listagem/detalhes-page/detalhes-page.component.ts
+++ front/src/app/pages/listagem/detalhes-page/detalhes-page.component.ts
@@ -5,10 +5,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { PostagemService } from '../../../services/postagem.service';
 import { SafeUrlPipe } from '../../../pipes/safe-url.pipe';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-detalhes-page',
-  imports: [NavbarComponent,CommonModule,SafeUrlPipe],
+  imports: [NavbarComponent,CommonModule],
   templateUrl: './detalhes-page.component.html',
   styleUrl: './detalhes-page.component.css',
   
@@ -17,6 +18,8 @@ export class DetalhesPageComponent {
 
   router= inject(Router);
   route = inject(ActivatedRoute);
+
+  sanitizer: DomSanitizer = inject(DomSanitizer);
 
   categoria: string = '';
 
@@ -34,6 +37,12 @@ export class DetalhesPageComponent {
     this.postagemService.buscarPostagemPorId(id).subscribe((data) => {
       this.postagem = data;
     });
+  }
+
+  getYoutubeEmbedUrl(link: string): SafeResourceUrl {
+    const videoId = link.match(/(?:youtu\.be\/|youtube\.com\/(?:watch\?v=|embed\/|v\/|shorts\/|.+\?v=))([^&?\/\s]+)/);
+    const embedUrl = videoId ? `https://www.youtube.com/embed/${videoId[1]}` : '';
+    return this.sanitizer.bypassSecurityTrustResourceUrl(embedUrl);
   }
 
 

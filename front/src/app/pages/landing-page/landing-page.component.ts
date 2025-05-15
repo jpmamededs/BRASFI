02a7aafc8b81d05f3a5edb1af04958a7667a4df2
MatheusLustosa <matheusclc@hotmail.com>
2025-05-15 15:18:16 -0300

    landpage quase pronta

--- front/src/app/pages/landing-page/landing-page.component.ts
+++ front/src/app/pages/landing-page/landing-page.component.ts
@@ -4,10 +4,11 @@ import { BigCardComponent } from "./components/big-card/big-card.component";
 import { DepoimentosComponent } from "./components/depoimentos/depoimentos.component";
 import { SmallCardComponent } from "./components/small-card/small-card.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { ContactComponent } from "../contact/contact.component";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [ SmallCardComponent, BigCardComponent, DepoimentosComponent, FooterComponent,FooterComponent],
+  imports: [SmallCardComponent, BigCardComponent, DepoimentosComponent, FooterComponent, FooterComponent, ContactComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
@@ -17,6 +18,16 @@ export class LandingPageComponent {
   imgSource:string="assets/images/Group.png";
   imgMulher2:string="assets/images/mulher2.svg";
   imgVector:string="assets/images/Vector.svg";
+  imgVoluntario:string="assets/images/voluntario.svg";
+  // parceiros
+  // imgSource:string="assets/images/feed.png";
+  // imgSource:string="assets/images/feed.png";
+  // imgSource:string="assets/images/feed.png";
+  // imgSource:string="assets/images/feed.png";
+  // imgSource:string="assets/images/feed.png";
+  // imgSource:string="assets/images/feed.png";
+  // imgSource:string="assets/images/feed.png";
+
 
   onCardClick(id: string | null) {
     this.selectedId = id;
